b3c57c86532da5fbb158187de089898a9d38eeeb
Roman Pudashkin <r.pudashkin@gmail.com>
2025-04-25 14:32:17 +0300

    added inputProcessingProgress + cleanup

--- src/framework/audio/audiotypes.h
+++ src/framework/audio/audiotypes.h
@@ -34,6 +34,7 @@
 #include "global/realfn.h"
 #include "global/async/channel.h"
 #include "global/io/iodevice.h"
+#include "global/progress.h"
 
 #include "mpe/events.h"
 
@@ -435,6 +436,16 @@ enum class RenderMode {
     RealTimeMode,
     IdleMode,
     OfflineMode
+};
+
+struct InputProcessingProgress {
+    struct ChunkInfo {
+        secs_t start = 0.0;
+        secs_t end = 0.0;
+    };
+
+    async::Channel<std::vector<ChunkInfo> > chunksBeingProcessedChannel;
+    Progress progress;
 };
 }
 
