6d7822e35b16bcd7cb705f6cccad126e5b09a0fe
Roman Pudashkin <r.pudashkin@gmail.com>
2026-06-24 17:54:54 +0300

    Implement automation change notifications

--- src/notationscene/qml/MuseScore/NotationScene/notationautomationcontroller.cpp
+++ src/notationscene/qml/MuseScore/NotationScene/notationautomationcontroller.cpp
@@ -26,6 +26,8 @@
 #include "engraving/automation/iautomation.h"
 #include "engraving/dom/masterscore.h"
 
+#include "defer.h"
+
 using namespace mu::notation;
 using namespace muse::uicomponents;
 
@@ -295,6 +297,11 @@ void NotationAutomationController::requestEditPoint(const PointData& oldPointDat
     // STEP 2 - Update the point value using the y parameter...
     // TODO: Not always dynamics...
     const mu::engraving::AutomationCurveKey curveKey { mu::engraving::AutomationType::Dynamics, staff->id(), /*voiceIdx*/ std::nullopt };
+
+    engravingAutomation()->beginTransaction();
+    DEFER {
+        engravingAutomation()->commitTransaction();
+    };
 
     //! NOTE: Point in/out values are always between 0 and 1 - higher value == lower Y...
     const double newValue = 1.0 - y;
